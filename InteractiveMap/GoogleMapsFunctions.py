dc3846b4b2a135e09b473faa16f8c4204c18aa5d
chakibMH <84680800+chakibMH@users.noreply.github.com>
2023-08-09 23:35:15 +0100

    Create GoogleMapsFunctions.py

---
+++ InteractiveMap/GoogleMapsFunctions.py
@@ -0,0 +1,41 @@
+import googlemaps
+
+import math
+
+def get_coordsPOI(POI, maps_api_key = "gmapsAPIKeys"):
+    
+    """this function will return the radians coordinates of a point of intrest (POI).
+    We'll use those coordinates to show them on our interactive map.
+    """
+
+    # client object
+    client = googlemaps.Client(key = maps_api_key)
+    
+    res = client.geocode("POI")[0]
+    
+    rad_coords = res['geometry']['location']
+    
+    return rad_coords
+
+
+def get_directions(api_key, origin, destination):
+  
+    gmaps = googlemaps.Client(key=api_key)
+    
+    try:
+        directions_result = gmaps.directions(origin, destination, mode="driving")
+        
+        if directions_result:
+            route = directions_result[0]
+            steps = route['legs'][0]['steps']
+            directions = [step['html_instructions'] for step in steps]
+            return directions
+        else:
+            print("No directions found.")
+    except Exception as e:
+        print("Error:", e)
+    
+    return None
+
+
+
